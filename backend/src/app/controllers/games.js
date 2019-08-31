64588e4263eff250d352439faa1494d956071c3a
Matheus Porto <matheusportoo@gmail.com>
2019-08-31 18:47:38 -0300

    Verifying if exists games before to read again the file log

--- backend/src/app/controllers/games.js
+++ backend/src/app/controllers/games.js
@@ -5,26 +5,36 @@ const ParserLog = require('../../utils/parser-log/index');
 const { pathLog, encodingLog } = require('../../config/log');
 
 const readFileAsync = promisify(fs.readFile)
+let games = [];
 
 module.exports = {
   async index(request, response) {
-    const log = await readFileAsync(pathLog, { encoding: encodingLog });
-    const parser = new ParserLog(log);
-    const games = parser.getGames();
+    if (!games.length) {
+      const log = await readFileAsync(pathLog, { encoding: encodingLog });
+      const parser = new ParserLog(log);
+
+      games = parser.getGames();
+    }
 
     return response.json(games);
   },
 
   async show(request, response) {
     const { id } = request.params;
-    const log = await readFileAsync(pathLog, { encoding: encodingLog });
-    const parser = new ParserLog(log);
-    const games = parser.getGames();
+
+    if (!games.length) {
+      const log = await readFileAsync(pathLog, { encoding: encodingLog });
+      const parser = new ParserLog(log);
+
+      games = parser.getGames();
+    }
 
     const game = games.find(item => Object.keys(item)[0] === `game_${id}`);
 
     if (!game) {
-      return response.status(400).json({ error: `Game ${id} not found! There are only ${games.length}` });
+      return response.status(400).json({
+        error: `Game ${id} not found! There are only ${games.length}`
+      });
     }
 
     return response.json(game);
