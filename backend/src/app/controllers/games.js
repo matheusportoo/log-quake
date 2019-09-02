b85581ebc2e90f510a7bcd445c71a05d45afa281
Matheus Porto <matheusportoo@gmail.com>
2019-09-01 22:06:20 -0300

    Adding validation in controller show

--- backend/src/app/controllers/games.js
+++ backend/src/app/controllers/games.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { promisify } = require('util');
+const Joi = require('@hapi/joi');
 
 const ParserLog = require('../../utils/parser-log/index');
 const { pathLog, encodingLog } = require('../../config/log');
@@ -8,7 +9,7 @@ const readFileAsync = promisify(fs.readFile)
 let games = [];
 
 module.exports = {
-  async index(request, response) {
+  async all(request, response) {
     if (!games.length) {
       const log = await readFileAsync(pathLog, { encoding: encodingLog });
       const parser = new ParserLog(log);
@@ -20,6 +21,16 @@ module.exports = {
   },
 
   async show(request, response) {
+    const schema = {
+      id: Joi.number().integer().min(1)
+    };
+
+    const { error } = Joi.validate(request.params, schema);
+
+    if (error) {
+      return response.status(400).json({error: error.details[0].message});
+    }
+
     const { id } = request.params;
 
     if (!games.length) {
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,7 +3,7 @@ const GamesController = require('./app/controllers/games');
 
 const routes = new Router();
 
-routes.get('/games', GamesController.index);
+routes.get('/games', GamesController.all);
 routes.get('/games/:id', GamesController.show);
 
 module.exports = routes;
